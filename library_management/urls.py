67ba21edd1c32e55b58cd9514478f1397b090caa
Nazrul islam <nazrulislamsuton06@gmail.com>
2023-10-27 00:52:36 +0600

    updated-some-file

--- library_management/urls.py
+++ library_management/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',views.home, name='home'),
+    path('',views.home, name='home'),
     path('account/',include('accounts.urls')),
     path('library/',include('library.urls')),
     path('borrowing/',include('borrowing_and_returning.urls'))
